c2d16307f3cb9684dff3538d0efeb15dc8d8f222
Skeedy <pierre_tisserand@orange.fr>
2019-05-16 13:48:31 +0200

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MatIconModule, MatToolbarModule, MatSidenavModule, MatCheckboxModule,
-  MatButtonModule, MatGridListModule, MatListModule } from '@angular/material';
+  MatButtonModule, MatGridListModule, MatListModule, MatMenuModule,
+  MatCardModule, MatFormFieldModule, MatInputModule} from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { ProductComponent } from './page/product/product.component';
 import { MenuComponent } from './page/menu/menu.component';
 import { Globals } from './globals';
 import { PopComponent } from './page/pop/pop.component';
+import { LoginComponent } from './page/login/login.component';
 
 const appRoutes: Routes = [
   { path: 'product', component: ProductComponent, data : { title: 'Produits JPEG'} },
   { path: 'menu', component: MenuComponent, data : { title: 'Menu JPEG'} },
   { path: 'pop', component: PopComponent, data : { title: 'Pop JPEG'} },
+  { path: 'login', component: LoginComponent, data : { title: 'loginPEG'} },
   { path: '', redirectTo: '/product', pathMatch: 'full' },
   { path: '**', component: AppComponent}
 ];
@@ -27,9 +30,13 @@ const appRoutes: Routes = [
     AppComponent,
     ProductComponent,
     MenuComponent,
-    PopComponent
+    PopComponent,
+    LoginComponent
   ],
   imports: [
+    MatInputModule,
+    MatCardModule,
+    MatMenuModule,
     MatGridListModule,
     FormsModule,
     BrowserModule,
@@ -41,7 +48,9 @@ const appRoutes: Routes = [
     MatCheckboxModule,
     MatButtonModule,
     RouterModule.forRoot(appRoutes),
-    MatListModule
+    MatListModule,
+    ReactiveFormsModule,
+    MatFormFieldModule
   ],
   providers: [
     Globals
