dc58613482a9399eda1a381229e360b5a505f394
solutionsSlayer <ddoussain@gmail>
2019-05-21 19:59:19 +0200

    new

--- src/app/page/type/type.component.ts
+++ src/app/page/type/type.component.ts
@@ -1,40 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Type } from '../../class/type';
-import { TypeService } from '../../service/type.service';
+import {Component, OnInit} from '@angular/core';
+import {Type} from '../../class/type';
+import {TypeService} from '../../service/type.service';
+import {Observable, of} from 'rxjs';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Router} from '@angular/router';
+import {catchError, tap} from 'rxjs/operators';
+import {Globals} from '../../globals';
 
 @Component({
   selector: 'app-type',
   templateUrl: './type.component.html',
   styleUrls: ['./type.component.scss']
 })
-export class TailleComponent implements OnInit {
-  error: boolean;
-  type: Type[];
-  loading: boolean;
-  types: any;
+export class TypeComponent implements OnInit {
+  uri = Globals.APP_API + 'type/';
+  types: Type[];
 
-  constructor(private typeService: TypeService, private router: Router) { }
+  constructor(private typeService: TypeService, private router: Router, private http: HttpClient) {
+  }
 
   ngOnInit() {
-    this.getType();
-  }
-  getAllTailles() {
-    this.typeService.getTypes()
-        .subscribe((data: Type[]) => {
-          this.type = data;
-        });
+    this.getTypes();
   }
 
-  deleteType(id: number) {
-    this.loading = true;
-    this.typeService.deleteType(id)
-        .subscribe(() => {
-          this.getAllTypes();
-        });
+  private log(log: string) {
+    // tslint:disable-next-line:no-console
+    console.info(log);
   }
 
-  createType() {
-    this.router.navigate(['type/add']);
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.log(error);
+      console.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
   }
+
+  getTypes() {
+    this.typeService.getTypes().subscribe(data => {
+      this.types = data;
+    });
+  }
+
+  delete(type: Type): void {
+    this.typeService.deleteType(type)
+        .subscribe(_ => this.getTypes());
+  }
+
 }
