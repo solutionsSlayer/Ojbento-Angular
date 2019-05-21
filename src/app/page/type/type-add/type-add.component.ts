5cf50f10d0cdac1e5932cdfea80772b7f5879ea5
solutionsSlayer <ddoussain@gmail>
2019-05-21 19:58:16 +0200

    new

--- src/app/page/type/type-add/type-add.component.ts
+++ src/app/page/type/type-add/type-add.component.ts
@@ -1,30 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { TypeService } from '../../../service/type.service';
+import {TypeService} from '../../../service/type.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Type} from '../../../class/type';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-type-add',
     templateUrl: './type-add.component.html',
     styleUrls: ['./type-add.component.scss']
 })
-
 export class TypeAddComponent implements OnInit {
-    public typeForm: FormGroup;
-    constructor(private fb: FormBuilder, private typeService: TypeService, private router: Router) { }
+
+    typeForm: FormGroup;
+    constructor(private typeService: TypeService, private fb: FormBuilder, private router: Router) { }
 
     ngOnInit() {
         this.typeForm = this.fb.group({
-            name : ['', Validators.required ]
+            name: [null, Validators]
         });
     }
+
     createType() {
         const val = this.typeForm.value;
-        this.typeService.createType(val.name).subscribe(() => {
-            this.router.navigate(['/type']);
-        });
-    }
-    backclicked() {
-        this.router.navigate(['/type']);
+        this.typeService.createType(val.name)
+            .subscribe( (type: Type) => {
+                this.router.navigate(['/type']);
+            });
     }
 }
