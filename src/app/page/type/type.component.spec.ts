55f27f5f773657d098ef95e5bf98450a60884dd0
solutionsSlayer <ddoussain@gmail>
2019-05-21 21:12:05 +0200

    new

---
+++ src/app/page/type/type.component.spec.ts
@@ -0,0 +1,27 @@
+// @ts-ignore
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+// @ts-ignore
+import { TypeComponent } from './type.component';
+
+describe('TypeComponent', () => {
+    let component: TypeComponent;
+    let fixture: ComponentFixture<TypeComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ TypeComponent ]
+        })
+            .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TypeComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
